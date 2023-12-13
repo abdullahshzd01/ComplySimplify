022b99e0279868c26ab33288ee8b052b5541159e
Sahil Parkash <sahiloutlook1962@gmail.com>
2023-12-13 23:41:41 +0500

    charts added

---
+++ Desktop/Chart/ComplySimplify-Abdullah/Frontend/src/Components/SignUpForm/Sign-up-form.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { FaUser, FaEnvelope, FaLock } from "react-icons/fa"; // Import icons from react-icons/fa
+import "./Sign-up-form.css"; // Import the CSS file
+import axios from "axios";
+
+function SignupForm() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [ErrorMsg, setErrorMsg] = useState("");
+
+    const handleSubmit = async (e) => {
+        // Handle form submission logic here
+        // You can access the form values using the state variables (name, email, password, confirmPassword)
+        // Perform validation, API calls, sor any other necessary operations
+        e.preventDefault();
+        console.log(name, email, password);
+
+        if (password.length < 6) {
+            alert("The password must be at least 6 characters.");
+            return;
+        }
+
+        try {
+            const response = await axios.post('http://localhost:4069/signup', { name, email, password });
+            console.log(response.data);
+
+            if (response.data.message === "User created successfully") {
+                alert("User created successfully");
+                window.location.href = "/signin";
+            }
+            else {
+                alert("Some Error");
+            }
+        } catch (error) {
+            console.error(error);
+            // Handle error, show an error message, etc.
+            setErrorMsg(error)
+        }
+
+        // axios.post("http://localhost:4000/signup", userObject)
+        //     .then((res) => console.log(res.data));
+
+
+
+        // alert("Successfully Registered, Please login now.");
+        // window.location.href = "/login";
+    };
+
+    return (
+        <div>
+            <h3 className="signup-form-heading-text">Sign Up with Email</h3>
+
+            <form onSubmit={handleSubmit} className="signup-form">
+                <div className="form-group">
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                        placeholder="Enter your Name"
+                    />
+                    <div className="icon">
+                        <FaUser />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        placeholder="Enter your Email"
+                    />
+                    <div className="icon">
+                        <FaEnvelope />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        placeholder="Enter your Password"
+                    />
+                    <div className="icon">
+                        <FaLock />
+                    </div>
+                </div>
+                <b>{ErrorMsg}</b>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignupForm;
---
+++ Desktop/Chart/ComplySimplify-Abdullah/Frontend/src/Screens/Internal-Working/EvidenceTasks/EvidenceTasks.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+import Logo from "../../../assets/Logo.png";
+// import tempImg from "../EvidenceTasks/temp.png";
+import "./EvidenceTasks-style.css";
+import NavigationEntities from "../../../Components/Navigation/Navigation_Entities";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import Table from "../../../Components/Tables/Table";
+import jsonData from "./data.json";
+// import { getAuth } from "firebase/auth";
+
+
+function EvidenceTasks() {
+
+    // const auth = getAuth();
+
+    // console.log("EvidenceTasks => auth: ", auth);
+    // console.log("EvidenceTasks => auth.lastNotifiedUid: ", auth.lastNotifiedUid);
+
+    // const To_SignIn = useNavigate();
+    // const NavigateToSignIn = () => {
+    //     // Handle your logout logic here
+    //     To_SignIn("/signin");
+    // };
+
+    // useEffect(() => {
+    //     // This code will run on page load
+
+    //     // Check conditions
+    //     if (!auth.lastNotifiedUid) {
+    //         // Execute code if the condition is true
+    //         console.log('No user logged in');
+    //         NavigateToSignIn();
+    //         alert("Login to continue");
+    //         return;
+    //     }
+
+    //     return () => {
+    //         // Cleanup code
+    //     };
+    // }, []);
+
+    const headers = ['Evidence Tasks', 'Assignee', 'Status', 'Action'];
+
+
+    // const To_dashboard = useNavigate();
+    // const navigateTo_dashboard = () => {
+    //     To_dashboard("/dashboard");
+    // };
+
+    // const To_standards = useNavigate();
+    // const navigateTo_standards = () => {
+    //     To_standards("/standards");
+    // };
+
+    // const To_EvidenceTasks = useNavigate();
+    // const navigateTo_EvidenceTasks = () => {
+    //     To_EvidenceTasks("/EvidenceTasks");
+    // };
+
+    // const To_People = useNavigate();
+    // const navigateTo_People = () => {
+    //     To_People("/People");
+    // };
+
+    // const To_Policy = useNavigate();
+    // const navigateTo_Policy = () => {
+    //     To_Policy("/Policy");
+    // };
+
+    // const To_Settings = useNavigate();
+    // const navigateTo_Settings = () => {
+    //     To_Settings("/Settings");
+    // };
+
+    // const To_SignUp = useNavigate();
+    // const handleLogout = () => {
+    //     // Handle your logout logic here
+    //     To_SignUp("/SignUp");
+    // };
+
+    return (
+        <div className="EvidenceTasks-container">
+            <div className="EvidenceTasks-left-pane">
+                <div>
+                    <img className="EvidenceTasks-logo-img" src={Logo} alt="Logo" />
+                </div>
+
+                <NavigationEntities />
+            </div>
+
+            <div className="EvidenceTasks-right-pane">
+                <div className="text">
+                    <h1 className="h1">Evidence tasks</h1>
+                </div>
+
+                <div>
+                    {/* <img className="TempImg" src={tempImg} alt="Temp" /> */}
+                    <Table headers={headers} data={jsonData} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EvidenceTasks;
